7383b9dc5d7792d4720317f7da62e409c5a08240
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-14 23:03:52 +0100

    added controller logic to add-card

--- src/controllers/alumnus-card.js
+++ src/controllers/alumnus-card.js
@@ -1,1 +1,14 @@
+const alumnusCardModel = require('../models/alumnus-card')
 
+module.exports = {
+    create: async (req, res, next) => {
+        const { body: { tag_no, institution_id, division_id, sub_division_id }, userId: alumnus_id = '30bee291-63e6-44d4-8cf1-7e331288779f' } = req
+
+        try {
+            const card = await alumnusCardModel.addOne(alumnus_id, tag_no, institution_id, division_id, sub_division_id)
+            res.status(201).json({ status: 'success', data: { message: 'Alma-mater successfully registered', card } })
+        } catch (e) {
+            next(e)
+        }
+    }
+}
