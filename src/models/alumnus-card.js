d29425f727d6a68971b95f931f6fc34db6cddc72
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-15 21:56:50 +0100

    added new form-fields

--- src/models/alumnus-card.js
+++ src/models/alumnus-card.js
@@ -1,13 +1,13 @@
 const { dbEntities } = require('../utils/helper')
-const { BadRequestError, DatabaseError } = require('../utils/http-errors')
+const { BadRequestError, DatabaseError, ConflictError } = require('../utils/http-errors')
 
 module.exports = {
-    async addOne(alumniId, tagNo, institutionId, divisionId, subDivisionId) {
-        if (!!await this.find(alumniId, institutionId)) throw new BadRequestError('This profile already exists')
-        if (!!await this.findByTagNo(tagNo, institutionId)) throw new BadRequestError('This registration number already exists')
+    async addOne(alumniId, tagNo, institutionId, divisionId, subDivisionId, admission_year, graduation_year) {
+        if (!!await this.find(alumniId, institutionId)) throw new ConflictError('This profile already exists')
+        if (!!await this.findByTagNo(tagNo, institutionId)) throw new ConflictError('This registration number already exists')
 
         try {
-            const input = [alumniId, tagNo, institutionId, divisionId, subDivisionId, '', '']
+            const input = [alumniId, tagNo, institutionId, divisionId, subDivisionId, admission_year, graduation_year]
             const result = await db.query(`INSERT INTO ${dbEntities.alumnus_cards} (alumnus_id, tag_no, institution_id, division_id, sub_division_id, admission_year, graduation_year) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id, tag_no, created_at`, input)
             return (result.rowCount === 0) ? null : result.rows[0]
         } catch (e) {
