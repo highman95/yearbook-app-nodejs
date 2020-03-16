67ae66a0cb37afc0dd49d1c6159ad94a68289425
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-16 12:54:19 +0100

    added route definition

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,6 @@
 const alumnusController = require('../controllers/alumnus');
 
 module.exports = (router) => {
+    router.post('/auth/signin', alumnusController.authenticate);
     router.post('/auth/register', alumnusController.create);
 }
